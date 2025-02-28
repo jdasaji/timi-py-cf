6b8bbef52cf180a08225b3902891a450ce1b2f41
jdasaji <jdasaji@gmail.com>
2025-02-28 14:17:15 -0500

    init 5

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -17,7 +17,7 @@ def get_secret():
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
-    )
+    ) 
 
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
